bf80be6162e3308d9551a9e2db7760771e63c804
임형섭 <hs.yim@illb.co.kr>
2022-11-19 15:07:36 +0900

    test - 다리 생성 테스트 추가

--- src/test/java/bridge/JunitTest.java
+++ src/test/java/bridge/JunitTest.java
@@ -1,10 +1,15 @@
 package bridge;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import org.assertj.core.api.AssertProvider;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -18,20 +23,33 @@ public class JunitTest {
         @Nested
         @DisplayName("입력 테스트")
         class input {
-            @Test
+            @ParameterizedTest
             @DisplayName("정상 입력")
             void valid_input() {
-                assertThat((AssertProvider<Integer>) () -> new InputView().readBridgeSize("3"))
+                assertThat((AssertProvider<Integer>) () -> new InputView().readBridgeSize())
                         .compareTo(3);
             }
             @Test
             @DisplayName("비정상 입력")
             void invalid_input() {
-                assertThatThrownBy(() -> new InputView().readBridgeSize("2"))
+                assertThatThrownBy(() -> new InputView().readBridgeSize())
                         .isInstanceOf(IllegalArgumentException.class)
                         .hasMessageContaining("[ERROR]");
             }
         }
+
+        @Nested
+        @DisplayName("기능 테스트")
+        class logic {
+            @Test
+            @DisplayName("다리 생성")
+            void generate_bridge() {
+                assertThat((AssertProvider<BridgeMaker>) () ->
+                        new BridgeMaker(() ->
+                                new BridgeRandomNumberGenerator().generate())).makeBridge(6);
+            }
+        }
+
 
     }
 
