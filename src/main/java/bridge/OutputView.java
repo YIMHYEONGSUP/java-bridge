ba541ce35860687f786bbf49d7142e541ab41a28
임형섭 <hs.yim@illb.co.kr>
2022-11-19 13:39:57 +0900

    fix - enum 값 -> 출력 메서드 변경

--- src/main/java/bridge/OutputView.java
+++ src/main/java/bridge/OutputView.java
@@ -11,29 +11,37 @@ public class OutputView {
      * 게임 시작 메세지 출력
      * */
     public void startMessage() {
-        System.out.println(START_GAME);
+        System.out.println(START_GAME.message());
     }
 
     /*
     * 다리 길이 입력 안내 문구 출력
     * */
     public void inputBridgeSize() {
-        System.out.println(INPUT_BRIDGE_SIZE);
+        System.out.println(INPUT_BRIDGE_SIZE.message());
+    }
+
+    /*
+    * 입력 받은 다리길이 출력
+    * */
+    public void inputtedBridgeSize(int bridgeSize) {
+        System.out.println(bridgeSize);
     }
 
     /*
     * 이동할 칸 입력 안내 문구 출력
     * */
     public void inputToMove() {
-        System.out.println(INPUT_TO_MOVE);
+        System.out.println(INPUT_TO_MOVE.message());
     }
 
     /*
     * 게임 재시작/종료 탐색 안내 문구 출력
     * */
     public void inputReGame() {
-        System.out.println(INPUT_REGAME);
+        System.out.println(INPUT_REGAME.message());
     }
+
 
 
 
