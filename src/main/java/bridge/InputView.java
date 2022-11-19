666e68927ff6d275ee55450331347cab11effabd
임형섭 <hs.yim@illb.co.kr>
2022-11-19 13:04:07 +0900

    feat - 다리 길이 입력,검증 기능

--- src/main/java/bridge/InputView.java
+++ src/main/java/bridge/InputView.java
@@ -1,5 +1,9 @@
 package bridge;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.regex.Pattern;
+
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  */
@@ -8,8 +12,27 @@ public class InputView {
     /**
      * 다리의 길이를 입력받는다.
      */
-    public int readBridgeSize() {
-        return 0;
+    public int readBridgeSize(String input) {
+        validation(input);
+        return Integer.parseInt(input);
+    }
+
+    /**
+    * 입력받은 다리 길이 검증.
+    * 조건 : 한자리 또는 두자리 숫자 , 3 이상 20 이하 의 숫자
+    * 정상 : 입력 받은 문자열 숫자로 변환 후 출력
+    * 비정상 : IllegalArgumentException 반환 , 에러메세지 '[ERROR]' 출력
+    * */
+    private void validation(String input) {
+        if (! Pattern.matches("\\d?\\d", input)) {
+            System.out.println(ExceptionMessages.INVALID_BRIDGE_SIZE.message());
+            throw new IllegalArgumentException("[ERROR]");
+        }
+        int bridgeSize = Integer.parseInt(input);
+        if (bridgeSize > 20 || bridgeSize < 3) {
+            System.out.println(ExceptionMessages.INVALID_BRIDGE_SIZE.message());
+            throw new IllegalArgumentException("[ERROR]");
+        }
     }
 
     /**
