94b3ca4b8d5210840822cfcfdb7e7e54c1d3b021
임형섭 <hs.yim@illb.co.kr>
2022-11-20 15:53:42 +0900

    feat - 사용자 칸 이동 기능 추가

--- src/main/java/bridge/BridgeGame.java
+++ src/main/java/bridge/BridgeGame.java
@@ -1,5 +1,7 @@
 package bridge;
 
+import java.util.List;
+
 /**
  * 다리 건너기 게임을 관리하는 클래스
  */
@@ -10,7 +12,25 @@ public class BridgeGame {
      * <p>
      * 이동을 위해 필요한 메서드의 반환 타입(return type), 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
      */
-    public void move() {
+    public boolean move(List<String> bridge , String move , int index) {
+
+        if (! validationMove(move)) {
+            throw new IllegalArgumentException("[ERROR]");
+        }
+
+        String now = bridge.get(index);
+        if (now.equals(move)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    private boolean validationMove(String move) {
+        if (move.equals("U") || move.equals("D")) {
+            return true;
+        }
+        return false;
     }
 
     /**
