796b64c17966b685c54836b7e193333991082ca6
YIMHYEONGSUP <hs.yim@illb.co.kr>
2022-11-21 08:47:51 +0900

    fix - 메서드 길이 조정

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,70 +1,124 @@
 package bridge;
 
-import camp.nextstep.edu.missionutils.Console;
-
 import java.util.List;
-import java.util.regex.Pattern;
 
 public class Application {
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
-
-
         List<String> bridge = settingGame();
-
-        runGame(bridge);
-
+        int[] result = startGame(bridge);
+        printResult(bridge , result);
     }
 
-    private static void runGame(List<String> bridge ) {
-        InputView input = new InputView();
+    private static void printResult(List<String> bridge , int[] result) {
+        printSuccessOrFail(bridge , result);
+        if (result[1] == bridge.size()) {
+            System.out.println("게임 성공 여부 : 성공");
+        }
+        if (result[1] < bridge.size()) {
+            System.out.println("게임 성공 여부 : 실패");
+        }
+        System.out.println("총 시도한 횟수 : " + result[0]);
+    }
+
+    private static void printSuccessOrFail(List<String> bridge , int[] result) {
         OutputView outputView = new OutputView();
-        int index = 0;
-        int tryCount = 1;
+        System.out.println("최종 게임 결과");
+        outputView.printMap(bridge , result[0]);
+    }
 
+    private static int[] startGame(List<String> bridge) {
+        int count = 1;
+        int result = 0;
         while (true) {
-            outputView.inputToMove();
-            BridgeGame bridgeGame = new BridgeGame();
-            String move = input.readMoving();
-            outputView.inputtedToMove(move);
-            boolean checkSquare = bridgeGame.move(bridge, move, index++);
-
-            String reGame = "";
-            // 칸 움직이기
-            if (! checkSquare) {
-                outputView.inputReGame();
-                reGame = input.readGameCommand();
+            result = runGame(bridge);
+            if (result == bridge.size()) {
+                break;
             }
+            if (finishGame(result)) {
+                break;
+            }
+            count++;
+        }
+        return new int[] {count , result};
+    }
 
+    private static boolean finishGame(int result) {
+        inputReGame();
+        String reGame = ReGame();
+        if (reGame.equals("Q")) {
+            return true;
+        }
+        return false;
+    }
+
+    private static String ReGame() {
+        InputView inputView = new InputView();
+        return inputView.readGameCommand();
+    }
+
+    private static void inputReGame() {
+        OutputView outputView = new OutputView();
+        outputView.inputReGame();
+    }
+
+    private static int runGame(List<String> bridge) {
+        BridgeGame bridgeGame = new BridgeGame();
+        int index = 0;
+        while (true) {
+            String move = move();
+            boolean square = bridgeGame.move(bridge, move, index++);
+            printMove(bridge , index);
+            String reGame = invalidSquare(square);
             if(reGame.equals("R")){
                 index = 0;
-                tryCount++;
             }
-
-            // 움직인 칸 출력
-            if (index >= 1) {
-//                System.out.println(bridge.subList(0, index));
-                outputView.printMap(bridge , index);
-            }
-
             if (reGame.equals("Q") || index == bridge.size()) {
                 break;
             }
-
-
         }
+        return index;
+    }
 
-        System.out.println("최종 게임 결과");
-        outputView.printMap(bridge , index);
+    private static String invalidSquare(boolean square) {
+        OutputView outputView = new OutputView();
+        InputView inputView = new InputView();
+        if (!square) {
+            outputView.inputReGame();
+            return inputView.readGameCommand();
+        }
+        return "";
+    }
 
-        if (index == bridge.size()) {
-            System.out.println("게임 성공 여부 : 성공");
+    private static void printMove(List<String> bridge , int index) {
+        OutputView outputView = new OutputView();
+        // 움직인 칸 출력
+        if (index >= 1) {
+            outputView.printMap(bridge , index);
         }
-        if (index < bridge.size()) {
-            System.out.println("게임 성공 여부 : 실패");
-        }
-        System.out.println("총 시도한 횟수 : " + tryCount);
+    }
+
+    private static String move() {
+        printInputMove();
+        String move = inputMove();
+        inputtedMove(move);
+        return move;
+    }
+
+    private static void inputtedMove(String move) {
+        OutputView outputView = new OutputView();
+        outputView.inputtedToMove(move);
+    }
+
+    private static void printInputMove() {
+        OutputView outputView = new OutputView();
+        outputView.inputToMove();
+    }
+
+    private static String inputMove() {
+        InputView inputView = new InputView();
+        String move = inputView.readMoving();
+        return move;
     }
 
     private static List<String> settingGame() {
